f427e1c922c81080a193628fc53e57f4184bc0ce
sdmule <mulesaurabh45@gmail.com>
2026-09-07 11:35:45 +0530

    Other way using a class instead of a record

--- MiniOrderManagement.Application/Customers/Queries/GetCustomerById/GetCustomerByIdQuery.cs
+++ MiniOrderManagement.Application/Customers/Queries/GetCustomerById/GetCustomerByIdQuery.cs
@@ -5,3 +5,15 @@ namespace MiniOrderManagement.Application.Customers.Queries.GetCustomerById;
 
 public record GetCustomerByIdQuery(
     int CustomerId) : IRequest<CustomerDto?>;
+
+
+//Other way using a class instead of a record
+//public class GetCustomerByIdQuery : IRequest<CustomerDto?>
+//{
+//    public int CustomerId { get; }
+
+//    public GetCustomerByIdQuery(int customerId)
+//    {
+//        CustomerId = customerId;
+//    }
+//}
